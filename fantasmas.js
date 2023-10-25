3675b48f7abe7abad39df4206effe16da10d3460
Juan Miguel Castanos <jmcastanos@webinver.com>
2023-10-24 20:43:10 -0700

    arreglar error en fantasmas

--- fantasmas.js
+++ fantasmas.js
@@ -3,12 +3,6 @@
 function mostrarFanstama(){
     console.log("Buuuuuuu Soy un fantasma");
     /*
- aklsjdalskdj alskdj 'a;sojd[
-    pa skd;'
-    lask d[
-        p;'askd;' 
-        lkas;dl kas;'ldk a;'sldk as';lk
-    ]
- ]
+            Ahora si la funcionalidad es perfecta!!!!
     */
 }
