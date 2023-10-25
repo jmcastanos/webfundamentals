8be9fa4afda4bf549cd746e5aebff0113209de4b
Juan Miguel Castanos <jmcastanos@webinver.com>
2023-10-24 20:39:20 -0700

    Agregar fn para mostrar fantasma

---
+++ fantasmas.js
@@ -0,0 +1,14 @@
+/* Yo soy juan */
+
+function mostrarFanstama(){
+    console.log("Buuuuuuu Soy un fantasma");
+    /*
+ aklsjdalskdj alskdj 'a;sojd[
+    pa skd;'
+    lask d[
+        p;'askd;' 
+        lkas;dl kas;'ldk a;'sldk as';lk
+    ]
+ ]
+    */
+}
